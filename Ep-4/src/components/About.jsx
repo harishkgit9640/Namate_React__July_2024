c6e6b3974345c488373c089284e43963533b844c
Harish Kumar <harishstudio877@gmail.com>
2024-07-26 01:50:53 +0530

    githum api added for about page

--- Ep-4/src/components/About.jsx
+++ Ep-4/src/components/About.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const About = () => {
+    const [data, setData] = useState()
+    const URL = "https://api.github.com/users/harishkgit9640";
+
+    console.log(data);
+    useEffect(() => {
+        fetchApiData();
+    }, []);
+
+    const fetchApiData = async () => {
+        const res = await fetch(URL);
+        const resData = await res.json();
+        setData(resData);
+    }
+
     return (
-        <h1>About</h1>
+        <div className="about-page">
+            {/* <h1> Name : {data.name} </h1>
+            <h1> Designation : {data.bio} </h1>
+            <h2> Location : {data.location}  </h2>
+            <img src={data.avatar_url} alt="user-img" width={150} /> */}
+            <h1>
+                hello world {data.name}
+            </h1>
+        </div>
     )
 }
 
